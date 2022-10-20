542286dbdd5cd65f00d3997e3e8d8857305ccf2c
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-20 18:40:09 +0000

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* more_numbers -prints 10 times the numbers
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+			putchar(j);
+
+	}
+	putchar('\n');
+}
