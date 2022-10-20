f1d6c06f322db70e70a3095f877b34266c8c30ba
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-20 19:12:00 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code for ALX School students.
+ *@n: number of times to print.
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = '0'; i < 'n'; i++)
+
+		putchar('_');
+
+	putchar('\n');
+}
