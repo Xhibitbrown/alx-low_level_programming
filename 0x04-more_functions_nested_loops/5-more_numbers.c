ac86e40f13f3fe70e96087750fd10d8eafd6833c
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-20 18:52:07 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,17 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (j >= 10)
-				putchar(j);
 
+			if (i >= 10)
+			{
+				putchar((i / 10) + '0');
+			}
+			putchar((i % 10) + '0');
 		}
-		putchar(j);
+		putchar('\n');
 	}
-	putchar('\n');
 }
