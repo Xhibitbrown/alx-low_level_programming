38ecdae4d8e1dba27d6a6bbb8f54451d05000b21
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-20 13:32:06 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - the multiplies of 2
+*@a: The numbers to mulit
+*Description: multiplication
+*@b:The numbers to be printed
+*Return: Always sucessful
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
