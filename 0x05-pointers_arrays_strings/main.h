406949bfa644c3192a2ce6acc2b9e3ef78859822
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-25 02:04:41 +0000

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 *Header file for all prototypes
 *used in 0x05. C - Pointers, arrays and strings
 */
-
+void _puts(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
