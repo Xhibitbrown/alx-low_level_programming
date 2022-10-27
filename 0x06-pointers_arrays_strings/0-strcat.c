07aa09e6c9082f821188b3e74f00a1e14f5d39f6
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-27 21:05:40 +0000

    size_t n

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,7 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	for (j = 0; src[j] != '\0'; j++, i++)
-	{
-		dest[i] = src[j];
-	}
-	dest[i] = '\0';
-	puchar(dest);
-
-	return (dest);
+	int n = strcat(dest, src);
+	putchar('\0');
+               return (dest);
 }
