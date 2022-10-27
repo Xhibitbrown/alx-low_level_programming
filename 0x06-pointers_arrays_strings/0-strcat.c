b7b6d03df7141117fa196e20694520daeeb82d80
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-27 21:17:41 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,11 +5,30 @@
 *_strcat - print an contate
 *Return: Always return the place to concate after
 *Description: Concating pointered strings
+*@dest: concating on
+*@src: concating this
 */
 
 char *_strcat(char *dest, char *src)
 {
-	int n = strcat(dest, src);
-	putchar('\0');
-               return (dest);
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+
 }
