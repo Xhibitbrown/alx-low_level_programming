4eb12f7c9edc6269155d97536187a445c9025dcc
Xhibitbrown <kusiadubofour72@gmail.com>
2022-10-27 20:03:26 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,10 +12,19 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; dest[i] = '\0'; i++)
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-	for (j = 0; src[i] != '\0'; j++)
+	while (src[j] != '\0')
+	{
 		dest[i] += src[j];
+		i++;
+		j++;
+	}
+
 	dest += '\0';
+
 	return (dest);
 }
