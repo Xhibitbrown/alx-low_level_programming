7a20043415447d9fc533f02e7901a1f91ea8b873
Xhibitbrown <kusiadubofour72@gmail.com>
2022-11-15 04:46:21 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
 *_puts_recursion - Prints a string followed by a new line
 *@s: string
@@ -9,12 +8,13 @@
 */
 
 void _puts_recursion(char *s)
+
 {
 	if (*s == 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
-	putchar(*s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,27 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int main (void)
-{
+/**
+ * Header files for all prototype
+ * Use in 0x08. Recursion
+ */
+
 	int _putchar(char c);
-
 	void _puts_recursion(char *s);
-
 	void _print_rev_recursion(char *s);
-
 	int _strlen_recursion(char *s);
-
 	int factorial(int n);
-
 	int _pow_recursion(int x, int y);
-
 	int _sqrt_recursion(int n);
-
 	int is_prime_number(int n);
-
 	int is_palindrome(char *s);
-
 	int wildcmp(char *s1, char *s2);
 
-}
+
 #endif
